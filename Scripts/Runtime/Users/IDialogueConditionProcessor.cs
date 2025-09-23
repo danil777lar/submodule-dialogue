86d4ffbc1c63c747794bfc31a2c5c1f80167ead6
danya <danil777lar@gmail.com>
2025-09-23 19:52:50 -0300

    remove supported conditions accessor

--- Scripts/Runtime/Users/IDialogueConditionProcessor.cs
+++ Scripts/Runtime/Users/IDialogueConditionProcessor.cs
@@ -3,5 +3,4 @@ using UnityEngine;
 public interface IDialogueConditionProcessor
 {
     public bool TryCheckCondition(string condition, out bool result);
-    public string[] SupportedConditions { get; }
 }
